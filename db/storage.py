053dcb8d231ee3dc9783175ec5cc9c8fa38d496f
Terrunda <terrencegift49@gmail.com>
2026-03-02 21:57:32 +0300

    Implmented functionality for storage.py with functions to make it compatible with auth_cli.

--- db/storage.py
+++ db/storage.py
@@ -1,8 +1,6 @@
 import json # Using the in-built json package to store data in users.json
 import os #Using os to access files in other directories.
 
-
-
 my_path = os.path.abspath(os.path.dirname(__file__)) #This accesses the current absolute path of the device
 file_path = os.path.join(my_path, "../data/users.json") # The absolute path is combined with the relative path to user.json to actually read the file. Sourced from Stack Overflow
 
@@ -51,20 +49,62 @@ def append_data(data, filepath = file_path):
         json.dump(file_contents, file_to_write, indent=4)
         # Without appending, the entire data in the file would be replaced by the data argument.
 
-def user_exists():
-    pass
+# Function for reading data 
+def read_data(filepath = file_path):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            file_contents = json.load(file)
+            return file_contents
+    except (json.JSONDecodeError, IOError):
+        return {"user_list": []}
 
-def save_user():
-    pass
 
-def get_user():
-    pass
+# Function to save user to the users.json file.
+def save_user(user_dict, filepath=file_path):
+
+    data = read_data(filepath)
+    
+    username = user_dict.get("username", "unknown")
+    
+    created_user = {
+        f"user_{username}": {
+            "password_hash": user_dict.get("password_hash", ""),
+            "session": user_dict.get("session", {}),
+            "transactions": user_dict.get("transactions", []),
+            "income": user_dict.get("income", []),
+            "expense": user_dict.get("expense", [])
+        }
+    }
+    
+    # Append to the list and write back to the file
+    data["user_list"].append(created_user)
+
+    with open(filepath, 'w', encoding='utf-8') as file_to_write:
+        json.dump(data, file_to_write, indent=4)
+
+#Function to get user from the users.json file.
+def get_user(username, filepath = file_path):
+    data = read_data(filepath)
+    target_key = f"user_{username}"
+    
+  
+    for user_entry in data.get("user_list", []):
+        if target_key in user_entry:
+            return user_entry[target_key]
+        else:
+            return False # Boolean indicating user was not found.
+
+def user_exists(username):
+    if get_user(username) == False:
+        return False
+    else:
+        return True
 
 '''Expected data structure:
 
 users = {
     user_list = [
-        user_{username}: {
+        {username}: {
             password_hash: {password}
 
             session: {}
